5d6d7e8b8e0350f769064f0f140320457fab56ba
cuongnv <150224586+cuongnvvietis@users.noreply.github.com>
2024-10-16 18:59:49 +0700

    Create lenh.cmd

---
+++ Docs/OpenStack/General/lenh.cmd
@@ -0,0 +1,8 @@
+Bạn có thể kiểm tra tài nguyên còn lại bằng lệnh:
+bash
+Sao chép mã
+openstack hypervisor stats show
+Hoặc kiểm tra trạng thái của các máy chủ Compute:
+bash
+Sao chép mã
+openstack compute service list
