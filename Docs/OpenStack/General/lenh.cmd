ade72efa25286a9eae9eb6039f9cdd7a89612b74
cuongnv <150224586+cuongnvvietis@users.noreply.github.com>
2024-10-16 19:50:29 +0700

    Update lenh.cmd

--- Docs/OpenStack/General/lenh.cmd
+++ Docs/OpenStack/General/lenh.cmd
@@ -5,3 +5,11 @@ Bạn có thể kiểm tra tài nguyên còn lại bằng lệnh:
 Hoặc kiểm tra trạng thái của các máy chủ Compute:
 
     openstack compute service list
+service nova-compute restart
+service nova-compute status
+
+service neutron-linuxbridge-agent restart
+
+
+nano /var/log/nova/nova-scheduler.log    ------controller
+nano /var/log/nova/nova-compute.log       ------ compute
