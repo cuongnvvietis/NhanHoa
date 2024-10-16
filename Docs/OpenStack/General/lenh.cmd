c426f20b1440d927b525820c3ff232e78ddcf886
cuongnv <150224586+cuongnvvietis@users.noreply.github.com>
2024-10-16 19:02:06 +0700

    Update lenh.cmd

--- Docs/OpenStack/General/lenh.cmd
+++ Docs/OpenStack/General/lenh.cmd
@@ -1,8 +1,7 @@
 Bạn có thể kiểm tra tài nguyên còn lại bằng lệnh:
-bash
-Sao chép mã
-openstack hypervisor stats show
+
+    openstack hypervisor stats show
+
 Hoặc kiểm tra trạng thái của các máy chủ Compute:
-bash
-Sao chép mã
-openstack compute service list
+
+    openstack compute service list
